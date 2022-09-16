195d4288373d8a6678c13b9d65c6d176345f03aa
Joakim Taule Kartveit <joakimkartveit@gmail.com>
2022-09-16 15:31:39 +0200

    Added in logger

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,9 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import {logger} from "../utils/logger";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  logger.info('Hello from logger');
   return <Component {...pageProps} />
 }
 
