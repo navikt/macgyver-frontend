770a9fa8baa31fb28c1be5fae0ce981fdb8ecb53
Joakim Taule Kartveit <joakimkartveit@gmail.com>
2022-09-16 15:32:09 +0200

    Removed unused import

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-import {logger} from "../utils/logger";
 
 const Home: NextPage = () => {
   return (
