3dcf6b1293602525592c66a4bf8e63d1eda6ce1d
Joakim Taule Kartveit <joakimkartveit@gmail.com>
2022-09-29 14:56:42 +0200

    Added in delete legeerklæring

---
+++ src/components/Legeerklaering/SlettLegeerklaeringForm.tsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import { Button, TextField } from '@navikt/ds-react';
+
+import ConfirmationModal from '../ConfirmationModal/ConfirmationModal';
+
+import styles from './SlettLegeerklaeringForm.module.css';
+
+interface SlettLegeerklaeringFormProps {
+    onSubmit: (legeerklaeringId: string) => void;
+}
+
+const SlettLegeerklaeringForm = ({ onSubmit }: SlettLegeerklaeringFormProps): JSX.Element => {
+    const [legeerklaeringId, setlegeerklaeringId] = useState<string>('');
+    const [conformationModalOpen, setConformationModalOpen] = useState(false);
+
+    return (
+        <div>
+            <TextField
+                name="legeerklaeringId"
+                label="legeerklaeringId"
+                size="medium"
+                onChange={(event) => {
+                    setlegeerklaeringId(event.currentTarget.value);
+                }}
+            />
+            <Button
+                variant="primary"
+                size="medium"
+                className={styles.button}
+                onClick={() => {
+                    setConformationModalOpen(true);
+                }}
+            >
+                Slett
+            </Button>
+            <ConfirmationModal
+                message={`Er du sikker på at du vil slette legeerklaeringId med id: ${legeerklaeringId}`}
+                onCancel={() => {
+                    setConformationModalOpen(false);
+                }}
+                onOK={() => {
+                    onSubmit(legeerklaeringId);
+                    setConformationModalOpen(false);
+                }}
+                open={conformationModalOpen}
+            ></ConfirmationModal>
+        </div>
+    );
+};
+
+export default SlettLegeerklaeringForm;
---
+++ src/pages/legeerklaring/slettLegeerklaring/SlettLegeerklaring.tsx
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import { Alert, BodyShort, Loader } from '@navikt/ds-react';
+import { logger } from '@navikt/next-logger';
+
+import Innhold from '../../../components/Innhold/Innhold';
+import { withAuthenticatedPage } from '../../../auth/withAuth';
+import SlettLegeerklaeringForm from '../../../components/Legeerklaering/SlettLegeerklaeringForm';
+
+const LEGEERKLARING_URL = `/api/proxy/api/legeerklaering`;
+
+const SlettLegeerklaring = (): JSX.Element => {
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState<string | null>(null);
+    const [success, setSuccess] = useState(false);
+
+    return (
+        <Innhold>
+            <BodyShort>Sletter en legeerklaeringI</BodyShort>
+            <SlettLegeerklaeringForm
+                onSubmit={(legeerklaeringId) => {
+                    setIsLoading(true);
+                    setSuccess(false);
+                    deleteLegeerklaring(legeerklaeringId)
+                        .then(() => {
+                            setSuccess(true);
+                        })
+                        .catch((error) => {
+                            setError(error.message);
+                        })
+                        .finally(() => {
+                            setIsLoading(false);
+                        });
+                }}
+            />
+            {isLoading && !error && <Loader size="medium" />}
+            {success && <Alert variant="success">Legeerklaring slettet</Alert>}
+            {error && <Alert variant="error">Noe gikk feil ved sletting av legeerklaring</Alert>}
+        </Innhold>
+    );
+};
+export const getServerSideProps = withAuthenticatedPage();
+
+async function deleteLegeerklaring(legeerklaeringId: string): Promise<void> {
+    const response = await fetch(`${LEGEERKLARING_URL}/${legeerklaeringId}`, {
+        method: 'DELETE',
+    });
+    logger.info(`SlettLegeerklaring response status is: ${response.status} and statusText ${response.statusText}`);
+    if (!response.ok) {
+        throw new Error(`Httpstatus code is ${response.status}`);
+    }
+}
+
+export default SlettLegeerklaring;
